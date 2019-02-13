a2cf52441d07d381cd32663cd77235a89a76b123
Ivan Torso <ivan.torso@gmail.com>
2019-02-13 11:42:54 +0100

    convert tabs to spaces for better readability

--- src/components/boilerplate/boilerplate.component.js
+++ src/components/boilerplate/boilerplate.component.js
@@ -3,48 +3,47 @@
  */
 export default class BoilerplateComponent{
 
-	/**
-	 * @param  {string} hello
-	 */
-	constructor(hello){
-		this.setHello(hello);
-	}
+    /**
+     * @param  {string} hello
+     */
+    constructor(hello){
+        this.setHello(hello);
+    }
 
 
-	/**
-	 * Getter
-	 * @return {string}
-	 */
-	getHello(){
-		return this.hello;	
-	}
+    /**
+     * Getter.
+     * @return {string}
+     */
+    getHello(){
+        return this.hello;
+    }
 
 
-	/**
-	 * Setter
-	 * @param {string} hello
-	 */
-	setHello(hello){
+    /**
+     * Setter.
+     * @param {string} hello
+     */
+    setHello(hello){
+        // validation
+        if(typeof hello != 'string'){
+            throw new Error('BoilerplateComponent: Invalid argument `hello`');
+        }
 
-		// validation
-		if(typeof hello != 'string'){
-			throw new Error('BoilerplateComponent: Invalid argument `hello`');
-		}
-
-		this.hello = hello;
-	}
+        this.hello = hello;
+    }
 
 
-	/**
-	 * Render helper
-	 * @return {string}
-	 */
-	render(){
-		return `
-			<div class="boilerplate-component">
-				<h1>Hello ${this.getHello()}!</h1>
-			</div>
-		`;
-	}
+    /**
+     * Render helper.
+     * @return {string}
+     */
+    render(){
+        return `
+            <div class="boilerplate-component">
+                <h1>Hello ${this.getHello()}!</h1>
+            </div>
+        `;
+    }
 
 };
--- webpack.config.js
+++ webpack.config.js
@@ -4,107 +4,107 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const WebpackNotifierPlugin = require('webpack-notifier');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
 
-	// https://webpack.js.org/concepts/entry-points/#object-syntax
-	entry: [
-		'./src/app.js', 
-		'./src/app.scss'
-	],
+    // https://webpack.js.org/concepts/entry-points/#object-syntax
+    entry: [
+        './src/app.js', 
+        './src/app.scss'
+    ],
 
-	// https://webpack.js.org/configuration/output/#output-library
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	},
+    // https://webpack.js.org/configuration/output/#output-library
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
 
-	// https://webpack.js.org/concepts/mode/#usage
-	mode: 'production',
+    // https://webpack.js.org/concepts/mode/#usage
+    mode: 'production',
 
-	// https://webpack.js.org/concepts/loaders/
-	module: {
-		rules: [
+    // https://webpack.js.org/concepts/loaders/
+    module: {
+        rules: [
 
-			{
-				// http://ccoenraets.github.io/es6-tutorial/setup-webpack/
-				// Babel loader
-				test: /\.js/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: [
-							'@babel/preset-env'
-						],
-						plugins: [
-							'@babel/plugin-proposal-class-properties'
-						]
-					}
-				}
-			},
+            {
+                // http://ccoenraets.github.io/es6-tutorial/setup-webpack/
+                // Babel loader
+                test: /\.js/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties'
+                        ]
+                    }
+                }
+            },
 
-			{
-				// https://github.com/webpack-contrib/sass-loader
-				// compoile sass in css
-				test: /\.scss/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: ['css-loader', 'sass-loader']
-				})
-			},
+            {
+                // https://github.com/webpack-contrib/sass-loader
+                // compoile sass in css
+                test: /\.scss/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
 
-			{
-				// https://survivejs.com/webpack/loading/fonts/
-				// Font and images loader
-				test: /\.(ttf|eot|woff|woff2|jpg|png|gif)$/,
-				use: {
-					loader: 'file-loader'
-				}
-			}
+            {
+                // https://survivejs.com/webpack/loading/fonts/
+                // Font and images loader
+                test: /\.(ttf|eot|woff|woff2|jpg|png|gif)$/,
+                use: {
+                    loader: 'file-loader'
+                }
+            }
 
-		]
-	},
+        ]
+    },
 
 
-	// https://webpack.js.org/configuration/resolve/
-	resolve: {
-		alias: {
-			'@components': path.resolve(__dirname, 'src/components'),
-			'@shared': path.resolve(__dirname, 'src/shared')
-		}
-	},
+    // https://webpack.js.org/configuration/resolve/
+    resolve: {
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@shared': path.resolve(__dirname, 'src/shared')
+        }
+    },
 
-	// https://webpack.js.org/configuration/plugins/
-	plugins: [
+    // https://webpack.js.org/configuration/plugins/
+    plugins: [
 
-		// https://www.npmjs.com/package/webpack-notifier
-		new WebpackNotifierPlugin(),
+        // https://www.npmjs.com/package/webpack-notifier
+        new WebpackNotifierPlugin(),
 
-		// https://github.com/johnagan/clean-webpack-plugin
-		new CleanWebpackPlugin(['dist']),
+        // https://github.com/johnagan/clean-webpack-plugin
+        new CleanWebpackPlugin(['dist']),
 		
-		// gestione nomenclatura output file css
-		new ExtractTextPlugin({
-			filename: 'bundle.css'
-		}),
+        // gestione nomenclatura output file css
+        new ExtractTextPlugin({
+            filename: 'bundle.css'
+        }),
 
-		// https://github.com/NMFR/optimize-css-assets-webpack-plugin
-		new OptimizeCSSAssetsPlugin({
-		  assetNameRegExp: /\.css$/g,
-		  cssProcessor: require('cssnano'),
-		  cssProcessorPluginOptions: {
-		    preset: ['default', { discardComments: { removeAll: true } }],
-		  },
-		  canPrint: true
-		}),
+        // https://github.com/NMFR/optimize-css-assets-webpack-plugin
+        new OptimizeCSSAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+                preset: ['default', {discardComments: {removeAll: true}}]
+            },
+            canPrint: true
+        }),
 
-		// first static homepage
-		new HtmlWebpackPlugin({
-			title: 'Homepage',
-			filename: 'index.html'
-		})
+        // first static homepage
+        new HtmlWebpackPlugin({
+            title: 'Homepage',
+            filename: 'index.html'
+        })
 
-	]
+    ]
 };
